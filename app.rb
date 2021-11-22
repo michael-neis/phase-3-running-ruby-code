a7d8b3dbf97f85998c701da223c88ce2b435572e
Michael Neis <michael.neis12@gmail.com>
2021-11-22 09:52:25 -0600

    commit tree 656dd2396bfa430493c04c5d6486ed032e87b0d4

---
+++ app.rb
@@ -0,0 +1,23 @@
+# puts "Hello world!"
+
+# puts("Hello world! pt 2")
+
+# print "D"
+# print "u"
+# print "n"
+# print "e"
+# print " "
+# print "r"
+# print "ocked"
+# puts ""
+
+# puts [1, 23, 234]
+# p [1, 23, 234]
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+puts "Hello World!"
+
+print "Pass this test, please."
+
+p [1,2,3]
